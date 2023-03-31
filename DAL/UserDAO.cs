7c015c6b90fd4557b894288e6ab81f477fb18d60
Aither <cheese736@hotmail.com>
2023-04-01 01:20:50 +0800

    Add project files.

---
+++ DAL/UserDAO.cs
@@ -0,0 +1,29 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UserDAO:PostContext
+    {
+        public UserDTO GetUserWithUsernameAndPassword(UserDTO model)
+        {
+            UserDTO dto = new UserDTO();
+            T_User user = db.T_User.FirstOrDefault(row => row.Username == model.Username && row.Password == model.Password );
+            if (user != null && user.ID != 0) 
+            {
+                dto.ID = user.ID;
+                dto.Username = user.Username;
+                dto.Name = user.NameSurname;
+                dto.Password = user.Password;
+                dto.ImagePath = user.ImagePath;
+                dto.isAdmin = user.isAdmin;
+            }
+            return dto;
+        }
+    }
+}
